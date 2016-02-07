bde687f7921889c941e217f68dfe84244dc0250d
Ball-Man <francesco@BallMan.station>
2016-02-07 18:50:31 +0000

    Tests

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ dispatcher = updater.dispatcher
 
 def now_handler(bot, update):
 	fl_today = g2f(datetime.now().date())
-	text = "Today is\n  year:       %d\n  month:  %d %s\n  day:        %d\nin the flawless calendar."%(fl_today[0], fl_today[1], flawless_months[fl_today[1]], fl_today[2])
+	text = "It's the %dth day of %s(%d) %d."%(fl_today[2], flawless_months[fl_today[1]], fl_today[1], fl_today[0])
 	bot.sendMessage(chat_id=update.message.chat_id, text=text)
 
 if __name__ == "__main__":
